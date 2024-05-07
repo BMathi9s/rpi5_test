c792ce271a25c2ab4d120fda23e1b5ceb5757ef3
MathiasAavaa <138600494+MathiasAavaa@users.noreply.github.com>
2024-05-07 13:04:44 -0400

    test

--- rc_car/motor_test.py
+++ rc_car/motor_test.py
@@ -4,17 +4,21 @@ import time
 
 def main():
     motor1 = MotorDriver(17, 18)  # create a MotorDriver object with dir_pin=17 and pwm_pin=18
+    motor2 = MotorDriver(22, 27)
 
     try:
         for speed in range(0, 101):
             motor1.set_speed(speed/100)  # gradually increase speed from 0 to 1
+            motor2.set_speed(speed/100)
             time.sleep(0.1)  # wait for 0.1 seconds before next change
 
     except KeyboardInterrupt:  # trap a CTRL+C keyboard interrupt
-        motor1.set_speed(1)  # set speed to full speed
+        motor1.set_speed(0)  # stop motor
+        motor2.set_speed(0)
 
     finally:
         motor1.close()  # in case of unexpected exit, resets pin status (motor will go full speed after exiting)
+        motor2.close()
 
 if __name__ == "__main__":
     main()
