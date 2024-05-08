b9eeb5980d2a45f3a1485d824698488cb553c0a3
MathiasAavaa <138600494+MathiasAavaa@users.noreply.github.com>
2024-05-08 13:19:31 -0400

    af

--- rc_car/test_ps4.py
+++ rc_car/test_ps4.py
@@ -5,10 +5,14 @@ class MyController(Controller):
         Controller.__init__(self, **kwargs)
 
     def on_x_press(self):
-        print("Hello world")
+        print("X button pressed")
 
     def on_x_release(self):
-        print("Goodbye world")
+        print("X button released")
+
+    def stop_event_loop(self):
+        pass  # Overriding this method with an empty body will suppress the default event messages
+
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 controller.listen(timeout=60)
