1ac3b403d183004c2047c7d49d19c2d026dacac7
MathiasAavaa <138600494+MathiasAavaa@users.noreply.github.com>
2024-05-08 14:29:52 -0400

    adcva

--- rc_car/main.py
+++ rc_car/main.py
@@ -2,6 +2,8 @@
 from motor_driver import MotorDriver
 from pyPS4Controller.controller import Controller
 
+speed_c = 0.5
+
 class MyController(Controller):
 
     def __init__(self, **kwargs):
@@ -9,40 +11,40 @@ class MyController(Controller):
         self.motor1 = MotorDriver(17, 18)
         self.motor2 = MotorDriver(22, 27)
 
-    def on_left_stick_y_axis(self, value):
-        speed = value / 32768
-        direction = 1 if speed >= 0 else 0
-        self.motor1.set_speed(abs(speed))
-        self.motor1.set_dir(direction)
+    # def on_left_stick_y_axis(self, value):
+    #     speed = value / 32768
+    #     direction = 1 if speed >= 0 else 0
+    #     self.motor1.set_speed(abs(speed))
+    #     self.motor1.set_dir(direction)
 
-    def on_right_stick_y_axis(self, value):
-        speed = value / 32768
-        direction = 1 if speed >= 0 else 0
-        self.motor2.set_speed(abs(speed))
-        self.motor2.set_dir(direction)
+    # def on_right_stick_y_axis(self, value):
+    #     speed = value / 32768
+    #     direction = 1 if speed >= 0 else 0
+    #     self.motor2.set_speed(abs(speed))
+    #     self.motor2.set_dir(direction)
 
     def on_up_arrow_press(self):
-        self.motor1.set_speed(1)
+        self.motor1.set_speed(speed_c)
         self.motor1.set_dir(1)
-        self.motor2.set_speed(1)
+        self.motor2.set_speed(speed_c)
         self.motor2.set_dir(1)
 
     def on_down_arrow_press(self):
-        self.motor1.set_speed(1)
+        self.motor1.set_speed(speed_c)
         self.motor1.set_dir(0)
-        self.motor2.set_speed(1)
+        self.motor2.set_speed(speed_c)
         self.motor2.set_dir(0)
 
     def on_left_arrow_press(self):
-        self.motor1.set_speed(1)
+        self.motor1.set_speed(speed_c)
         self.motor1.set_dir(0)
-        self.motor2.set_speed(1)
+        self.motor2.set_speed(speed_c)
         self.motor2.set_dir(1)
 
     def on_right_arrow_press(self):
-        self.motor1.set_speed(1)
+        self.motor1.set_speed(speed_c)
         self.motor1.set_dir(1)
-        self.motor2.set_speed(1)
+        self.motor2.set_speed(speed_c)
         self.motor2.set_dir(0)
 
     def on_up_down_left_right_arrow_release(self):
